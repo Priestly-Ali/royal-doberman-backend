257dfc0e38eba684d8f1c5c1ec02c7fbc38e56c4
Priestly-Ali <alipriestly7@gmail.com>
2026-02-21 20:50:57 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Royal Doberman Empire Backend Running");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
